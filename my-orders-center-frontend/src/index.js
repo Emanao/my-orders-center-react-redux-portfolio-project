791a50a7683a5d8796ea98abcfa919a237524a4f
emanao <emanao@me.com>
2021-06-18 12:31:38 -0400

    Setup redux store and orders reducer

--- my-orders-center-frontend/src/index.js
+++ my-orders-center-frontend/src/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import {createStore, compose, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+
+import rootReducer from './reducers/orders'
 import App from './App';
+
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)));
+
+// import './index.css';
 // import reportWebVitals from './reportWebVitals';
 
+
 ReactDOM.render(
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
   ,document.getElementById('root')
 );
 
