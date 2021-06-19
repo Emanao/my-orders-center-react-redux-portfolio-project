c9d71398e8e03eaac25dcbb872243bc34a139da0
emanao <emanao@me.com>
2021-06-19 11:49:18 -0400

    Set state on change in OrderForm component.

--- my-orders-center-frontend/src/components/OrderForm.js
+++ my-orders-center-frontend/src/components/OrderForm.js
@@ -1,31 +1,64 @@
 import React from 'react'
 
 class OrderInput extends React.Component{
+    state={
+        nr: "",
+        site:"",
+        notes:""
+    }
+    handleChange = (event)=>{
+        this.setState({[event.target.name]:event.target.value})
+    }
     render(){
         return (
-            <div class="container">
+            <div className="container">
                 <h2>Add Order to Track</h2>
                 <form >
-                    <div class="form-group">
-                        <label for="order_id" class="col-form-label">Order number</label>
-                        <div class="col-sm-8">
-                        <input type="email" name="nr" class="form-control form-control-sm" id="order_id" placeholder=""/>
+                    <div className="form-group">
+                        <label htmlFor="order_id" className="col-form-label">Order number</label>
+                        <div className="col-sm-8">
+                        <input 
+                        className="form-control form-control-sm" 
+                        type="text" 
+                        name="nr"
+                        value={this.state.nr}
+                        onChange={this.handleChange}
+                        id="order_id" 
+                        placeholder=""/>
                         </div>
                     </div>
-                    <div class="form-group">
-                        <label for="site" class="col-form-label">Store</label>
-                        <div class="col-sm-8">
-                            <input type="email" name="site" class="form-control form-control-sm" id="site" placeholder=""/>
+                    <div className="form-group">
+                        <label htmlFor="site" className="col-form-label">Store</label>
+                        <div className="col-sm-8">
+                            <input 
+                            className="form-control form-control-sm" 
+                            type="text" 
+                            name="site"
+                            value={this.state.site} 
+                            onChange={this.handleChange}
+                            id="site" 
+                            placeholder=""/>
                         </div>
                     </div>
-                    <div class="form-group">
-                        <label for="order_notes" class="col-form-label"> Notes</label>
-                        <div class="col-sm-8">
-                            <textarea class="form-control form-control-sm" id="order_notes" rows="3"></textarea>
+                    <div className="form-group">
+                        <label htmlFor="order_notes" className="col-form-label"> Notes</label>
+                        <div className="col-sm-8">
+                            <textarea 
+                            className="form-control form-control-sm" 
+                            name="notes"
+                            value={this.state.notes}
+                            onChange={this.handleChange}
+                            id="order_notes" 
+                            rows="3">              
+                            </textarea>
                         </div>
                     </div>
-                    <div class="form-group mt-3">
-                    <button type="submit" class="btn btn-dark">Add Order</button>
+                    <div className="form-group mt-3">
+                        <button 
+                        type="submit" 
+                        className="btn btn-dark">
+                        Add Order
+                        </button>
                     </div>
                 </form>
             </div>
