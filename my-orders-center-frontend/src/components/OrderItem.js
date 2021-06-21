5088421c5d1694c16c6bb589d000120f8ca5b1f7
emanao <emanao@me.com>
2021-06-21 16:40:27 -0400

    Fix component name OrderItem

--- my-orders-center-frontend/src/components/OrderItem.js
+++ my-orders-center-frontend/src/components/OrderItem.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 // import { connect } from 'react-redux'
 
 const OrderItem = ({order})=>{
     return(
         <tr>
-            <th scope='row'>{order.attributes.nr}</th>
+            <th scope='row'>
+                <Link>{order.attributes.nr}</Link>
+                </th>
             <td>{order.attributes.description}</td>
             <td>{order.attributes.site.name}</td>
         </tr>
