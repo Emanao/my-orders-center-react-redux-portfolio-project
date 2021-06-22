d4a327efe0df6c08864b076a003376a51e533a0a
emanao <emanao@me.com>
2021-06-22 14:54:23 -0400

    Notes display in OrderDetails

--- my-orders-center-frontend/src/components/OrderDetails.js
+++ my-orders-center-frontend/src/components/OrderDetails.js
@@ -3,10 +3,13 @@ import React from 'react'
 const OrderDetails = ({order})=>{
     return (
         !!order?
-        <div>
-                <h3>{order.attributes.nr}</h3>
-                <p>{order.attributes.description}</p>
-                <p>{order.attributes.Site}</p>
+        <div className='container'>
+                <h3>Details of Order #: {order.attributes.nr}</h3>
+                <p>Description: {order.attributes.description}</p>
+                <p>Store: {order.attributes.site.name}</p>
+                <h4>Notes</h4>
+                {order.attributes.notes.map((note, idx)=><p key={idx}>{note.content}</p>)}
+
 
         </div>:null
         )
