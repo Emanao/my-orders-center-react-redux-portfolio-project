bbd1cf90a8299bc33fff1734a1f9c26439c0c0fd
emanao <emanao@me.com>
2021-06-28 19:48:16 -0400

    Add table and field created on in the notes presentation

--- my-orders-center-frontend/src/components/NoteCard.js
+++ my-orders-center-frontend/src/components/NoteCard.js
@@ -1,18 +1,24 @@
 import React from 'react'
 // 
 const NoteCard = (props)=>{
-    const { note, deleteNote} = props
+    const { deleteNote} = props;
+    const { id } = props.note;
+    const { content, created_at } = props.note.attributes;
+    const date = new Date(created_at);
 
-    const handleClick = ()=>deleteNote(note.id);
+    const handleClick = ()=>deleteNote(id);
     
     return (
-        <div> 
+        <tr>
+            <th scope="row">
             <button 
-            type="button" 
-            onClick={handleClick} 
-            className="btn btn-outline-dark btn-sm">X</button>  
-            <span className='ms-3'>{note.attributes.content}</span> 
-        </div>
+                type="button" 
+                onClick={handleClick} 
+                className="btn btn-outline-dark btn-sm">X</button>  
+            </th>
+            <td>{date.toLocaleDateString()}</td>
+            <td>{content}</td>
+        </tr>
     )
 }
 export default NoteCard;
--- my-orders-center-frontend/src/components/NotesContainer.js
+++ my-orders-center-frontend/src/components/NotesContainer.js
@@ -3,9 +3,14 @@ import React from 'react'
 import NotesForm from './NotesForm';
 import NoteCard from './NoteCard';
 
-// 
 const NotesContainer =(props)=>{
     const {order, notes, deleteNote, createNote} = props;
+    const notesList = notes.map(note=><NoteCard 
+                                        key={note.id} 
+                                        note={note} 
+                                        deleteNote={deleteNote}
+                                        />);
+
 
     return(            
         <div>
@@ -14,14 +19,18 @@ const NotesContainer =(props)=>{
             orderId={order.id}
             />
             <hr/>
-            <div className="row gx-5">
-                {notes.map(note=><div key={note.id}>
-                    <NoteCard 
-                        note={note} 
-                        deleteNote={deleteNote}
-                        />
-                </div>)}
-            </div>
+            <table className="table table-sm">
+                <thead>
+                    <tr>
+                        <th className="col-sm-1" scope="col"> </th>
+                        <th className="col-sm-2" scope="col">Created on</th>
+                        <th className="col-sm-9" scope="col">Notes</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {notesList}
+                </tbody>
+            </table>
         </div> 
     )
 }
